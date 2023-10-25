68ce312c6095126ca17f24635c2e0ef37d309ddc
Pedro Vitor Mantovani <83127263+Pedrinvits@users.noreply.github.com>
2023-10-24 21:28:36 -0300

    feat - sliders

--- src/app/components/programs.tsx
+++ src/app/components/programs.tsx
@@ -1,12 +1,13 @@
 "use client"
 import { Slider } from './slider'
-
+import { useMediaQuery  } from 'usehooks-ts'
 export const Programs = () => {
+    const isLarge = useMediaQuery('(min-width: 730px)')
     return (
         <section className="container flex flex-col">
             <h2 className="md:text-6xl text-4xl text-white">Not sure where start ?</h2>
             <p className="text-gray-500 mt-4 mb-2 md:text-lg text-sm">Programs offer day-to-day guidance on an interactive calendar to keep you on track</p>
-            <Slider sliders={1}/> 
+            {isLarge ? <Slider sliders={3}/> : <Slider sliders={1}/>}
         </section>
     )
 }
